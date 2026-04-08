40dc4000599598bfac932f21b6ef49e1808d2323
ankush <raneankush93@gmail.com>
2026-04-08 17:19:26 +0530

    test: verify_refresh_token test passing

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch, MagicMock
-from app.services.token_service import store_refresh_token
+from app.services.token_service import store_refresh_token, verify_refresh_token
+
 
 def test_store_refresh_token():
     mock_redis = MagicMock()
@@ -12,3 +13,13 @@ def test_store_refresh_token():
             60*60*24*7,
             "My_Token"
         )
+        
+def test_verify_refresh_token():
+    mock_redis = MagicMock()
+
+    with patch("app.services.token_service.get_redis", return_value = mock_redis):
+
+        mock_redis.get.return_value = "difftoken"
+        result = verify_refresh_token(1,"My_Token")
+        assert result == False
+    

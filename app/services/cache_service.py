4bf5a84efe23aed2cc34afcf379e97c243f99520
ankush <raneankush93@gmail.com>
2026-04-12 20:34:40 +0530

    feat : cache service

---
+++ app/services/cache_service.py
@@ -0,0 +1,28 @@
+from app.database.redis import get_redis
+
+def set_url(short_code, original_url, ttl:int):
+    redis = get_redis()
+    key = short_code
+    redis.setex(key,ttl,original_url)
+
+def get_url(short_code):
+    redis = get_redis()
+    key = short_code
+    return redis.get(short_code)
+
+def delete_url(short_code):
+    redis = get_redis()
+    key = short_code
+    redis.delete(key)
+
+def increment_click(short_code):
+    redis = get_redis()
+    key = short_code
+    redis.incr(f"{key}:clicks")
+    
+
+
+
+
+
+
